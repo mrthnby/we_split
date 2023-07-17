8c2caee022f148986f4c04d8880434ab0d9a4812
mrthnby <meralihanbay@gmail.com>
2023-07-17 23:06:19 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Mert Ali Hanbay on 17.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
